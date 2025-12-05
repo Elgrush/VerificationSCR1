2fe4b3329510841bf8d9b50d94b9d24d2d665b14
Elgrush <125126782+Elgrush@users.noreply.github.com>
2025-12-05 03:16:30 +0300

    wb master scr in making

---
+++ src/wb/utils/wb_master_scr.v
@@ -0,0 +1,79 @@
+`include "scr1_arch_description.svh"
+`include "scr1_arch_types.svh"
+`include "scr1_memif.svh"
+
+module wb_master_scr (
+    // Wishbone interfaces
+	input wb_rst_i,
+	input wb_clk_i,
+
+	output reg [31:0] wbm_adr_o,
+	output reg [31:0] wbm_dat_o,
+	input [31:0] wbm_dat_i,
+	output reg wbm_we_o,
+	output reg [3:0] wbm_sel_o,
+	output reg wbm_stb_o,
+	input wbm_ack_i,
+	output reg wbm_cyc_o,
+);
+    localparam IDLE = 2'b00;
+	localparam WBSTART = 2'b01;
+	localparam WBEND = 2'b10;
+
+	reg [1:0] state;
+
+	wire we;
+	assign we = (mem_wstrb[0] | mem_wstrb[1] | mem_wstrb[2] | mem_wstrb[3]);
+
+	always @(posedge wb_clk_i) begin
+		if (!wb_rst_n_i) begin
+			wbm_adr_o <= 0;
+			wbm_dat_o <= 0;
+			wbm_we_o <= 0;
+			wbm_sel_o <= 0;
+			wbm_stb_o <= 0;
+			wbm_cyc_o <= 0;
+			state <= IDLE;
+		end else begin
+			case (state)
+				IDLE: begin
+					if (mem_valid) begin
+						wbm_adr_o <= mem_addr;
+						wbm_dat_o <= mem_wdata;
+						wbm_we_o <= we;
+						wbm_sel_o <= mem_wstrb;
+
+						wbm_stb_o <= 1'b1;
+						wbm_cyc_o <= 1'b1;
+						state <= WBSTART;
+					end else begin
+						mem_ready <= 1'b0;
+
+						wbm_stb_o <= 1'b0;
+						wbm_cyc_o <= 1'b0;
+						wbm_we_o <= 1'b0;
+					end
+				end
+				WBSTART:begin
+					if (wbm_ack_i) begin
+						mem_rdata <= wbm_dat_i;
+						mem_ready <= 1'b1;
+
+						state <= WBEND;
+
+						wbm_stb_o <= 1'b0;
+						wbm_cyc_o <= 1'b0;
+						wbm_we_o <= 1'b0;
+					end
+				end
+				WBEND: begin
+					mem_ready <= 1'b0;
+
+					state <= IDLE;
+				end
+				default:
+					state <= IDLE;
+			endcase
+		end
+	end
+endmodule
--- src/wb/wb_scr1.sv
+++ src/wb/wb_scr1.sv
@@ -1,3 +1,7 @@
+`include "scr1_arch_description.svh"
+`include "scr1_arch_types.svh"
+`include "scr1_memif.svh"
+
 module wb_scr1 (
 	// Wishbone interfaces
 	input wb_rst_n_i,
@@ -29,109 +33,77 @@ module wb_scr1 (
 	output        trace_valid,
 	output [35:0] trace_data
 	
-);
-	wire        mem_valid;
-	wire [31:0] mem_addr;
-	wire [31:0] mem_wdata;
-	wire [ 3:0] mem_wstrb;
-	reg         mem_ready;
-	reg [31:0] mem_rdata;
+);	
+	logic imem2core_req_ack;        // IMEM request acknowledge
+	logic core2imem_req;            // IMEM request
+	logic [`SCR1_IMEM_AWIDTH-1:0] core2imem_addr;           // IMEM address
+	logic [`SCR1_IMEM_DWIDTH-1:0] imem2core_rdata;          // IMEM read data
+	type_scr1_mem_resp_e imem2core_resp;           // IMEM response
+
+	// Data Memory Interface
+	logic dmem2core_req_ack;        // DMEM request acknowledge
+	logic core2dmem_req;            // DMEM request
+	type_scr1_mem_cmd_e core2dmem_cmd;            // DMEM command
+	type_scr1_mem_width_e core2dmem_width;          // DMEM data width
+	logic [`SCR1_IMEM_AWIDTH-1:0] core2dmem_addr;           // DMEM address
+	logic core2dmem_wdata;          // DMEM write data
+	logic [`SCR1_IMEM_DWIDTH-1:0] dmem2core_rdata;          // DMEM read data
+	type_scr1_mem_resp_e dmem2core_resp;            // DMEM response
 
 	scr1_core_top scr1_core (
 
-    .pwrup_rst_n(),                // Power-Up reset
-    .rst_n(),                      // Regular reset
-    .cpu_rst_n(),                  // CPU reset
-    .test_mode(),                  // DFT Test Mode
-    .test_rst_n(),                 // DFT Test Reset
-    .clk(),                        // Core clock
-    .core_rst_n_o(),               // Core reset
-    .core_rdc_qlfy_o(),            // Core RDC qualifier
+		.pwrup_rst_n(wb_rst_n_i),                // Power-Up reset
+		.rst_n(wb_rst_n_i),                      // Regular reset
+		.cpu_rst_n(wb_rst_n_i),                  // CPU reset
+		.test_mode('0),                  // DFT Test Mode
+		.test_rst_n('0),                 // DFT Test Reset
+		.clk(wb_clk_i),                        // Core clock
+		.core_rst_n_o(wb_rst_n_i),               // Core reset
+		.core_rdc_qlfy_o('0),            // Core RDC qualifier
 
-// Instruction Memory Interface
-    .imem2core_req_ack_i(),        // IMEM request acknowledge
-    .core2imem_req_o(),            // IMEM request
-    .core2imem_cmd_o(),            // IMEM command
-    .core2imem_addr_o(),           // IMEM address
-    .imem2core_rdata_i(),          // IMEM read data
-    .imem2core_resp_i(),           // IMEM response
+		// Instruction Memory Interface
+		.imem2core_req_ack_i(imem2core_req_ack),        // IMEM request acknowledge
+		.core2imem_req_o(core2imem_req),            // IMEM request
+		.core2imem_cmd_o(core2imem_cmd),            // IMEM command
+		.core2imem_addr_o(core2imem_addr),           // IMEM address
+		.imem2core_rdata_i(imem2core_rdata),          // IMEM read data
+		.imem2core_resp_i(imem2core_resp),           // IMEM response
 
-// Data Memory Interface
-    .dmem2core_req_ack_i(),        // DMEM request acknowledge
-    .core2dmem_req_o(),            // DMEM request
-    .core2dmem_cmd_o(),            // DMEM command
-    .core2dmem_width_o(),          // DMEM data width
-    .core2dmem_addr_o(),           // DMEM address
-    .core2dmem_wdata_o(),          // DMEM write data
-    .dmem2core_rdata_i(),          // DMEM read data
-    .dmem2core_resp_i()            // DMEM response
+		// Data Memory Interface
+		.dmem2core_req_ack_i(dmem2core_req_ack),        // DMEM request acknowledge
+		.core2dmem_req_o(core2dmem_req),            // DMEM request
+		.core2dmem_cmd_o(core2dmem_cmd),            // DMEM command
+		.core2dmem_width_o(core2dmem_width),          // DMEM data width
+		.core2dmem_addr_o(core2dmem_addr),           // DMEM address
+		.core2dmem_wdata_o(core2dmem_wdata),          // DMEM write data
+		.dmem2core_rdata_i(dmem2core_rdata),          // DMEM read data
+		.dmem2core_resp_i(dmem2core_resp)            // DMEM response
 
-);
+	);
 
-	localparam IDLE = 2'b00;
-	localparam WBSTART = 2'b01;
-	localparam WBEND = 2'b10;
+	wb_master_scr instr_master (
+		.wb_rst_i(wb_rst_i),
+		.wb_clk_i(wb_clk_i),
 
-	reg [1:0] state;
+		.wbm_adr_o(wbm_adr_instr_o),
+		.wbm_dat_o(wbm_dat_instr_o),
+		.wbm_dat_i(wbm_dat_instr_i),
+		.wbm_we_o (wbm_we_instr_o),
+		.wbm_sel_o(wbm_sel_instr_o),
+		.wbm_stb_o(wbm_stb_instr_o),
+		.wbm_ack_i(wbm_ack_instr_i),
+		.wbm_cyc_o(wbm_cyc_instr_o)
+	), data_master(
+		.wb_rst_i(wb_rst_i),
+		.wb_clk_i(wb_clk_i),
 
-	wire we;
-	assign we = (mem_wstrb[0] | mem_wstrb[1] | mem_wstrb[2] | mem_wstrb[3]);
-
-	always @(posedge wb_clk_i) begin
-		if (!wb_rst_n_i) begin
-			wbm_adr_o <= 0;
-			wbm_dat_o <= 0;
-			wbm_we_o <= 0;
-			wbm_sel_o <= 0;
-			wbm_stb_o <= 0;
-			wbm_cyc_o <= 0;
-			state <= IDLE;
-		end else begin
-			case (state)
-				IDLE: begin
-					if (mem_valid) begin
-						wbm_adr_o <= mem_addr;
-						wbm_dat_o <= mem_wdata;
-						wbm_we_o <= we;
-						wbm_sel_o <= mem_wstrb;
-
-						wbm_stb_o <= 1'b1;
-						wbm_cyc_o <= 1'b1;
-						state <= WBSTART;
-					end else begin
-						mem_ready <= 1'b0;
-
-						wbm_stb_o <= 1'b0;
-						wbm_cyc_o <= 1'b0;
-						wbm_we_o <= 1'b0;
-					end
-				end
-				WBSTART:begin
-					if (wbm_ack_i) begin
-						mem_rdata <= wbm_dat_i;
-						mem_ready <= 1'b1;
-
-						state <= WBEND;
-
-						wbm_stb_o <= 1'b0;
-						wbm_cyc_o <= 1'b0;
-						wbm_we_o <= 1'b0;
-					end
-				end
-				WBEND: begin
-					mem_ready <= 1'b0;
-
-					state <= IDLE;
-				end
-				default:
-					state <= IDLE;
-			endcase
-		end
-	end
-endmodule
-
-module wb_master (
-    
-);
-    
+		.wbm_adr_o(wbm_adr_data_o),
+		.wbm_dat_o(wbm_dat_data_o),
+		.wbm_dat_i(wbm_dat_data_i),
+		.wbm_we_o (wbm_we_data_o), 	
+		.wbm_sel_o(wbm_sel_data_o),
+		.wbm_stb_o(wbm_stb_data_o),
+		.wbm_ack_i(wbm_ack_data_i),
+		.wbm_cyc_o(wbm_cyc_data_o)
+	);
 endmodule
